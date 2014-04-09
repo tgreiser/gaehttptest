a3bd76890aac3ff322a189607c812aabf6b24856
Tim Greiser <tgreiser@gmail.com>
2014-04-09 04:05:41 -0400

    export ResponseWriter

--- gaehttptest.go
+++ gaehttptest.go
@@ -10,55 +10,55 @@ import (
 	"appengine"
 )
 
-type responseWriter struct {
+type ResponseWriter struct {
 	StatusCode int
 	header     http.Header
 	Body       *bytes.Buffer
 }
 
-func newResponseWriter() *responseWriter {
-	return &responseWriter{
+func newResponseWriter() *ResponseWriter {
+	return &ResponseWriter{
 		StatusCode: 200,
 		header:     make(http.Header),
 		Body:       &bytes.Buffer{},
 	}
 }
 
-func (w *responseWriter) Header() http.Header {
+func (w *ResponseWriter) Header() http.Header {
 	return w.header
 }
 
-func (w *responseWriter) Write(b []byte) (int, error) {
+func (w *ResponseWriter) Write(b []byte) (int, error) {
 	return w.Body.Write(b)
 }
 
-func (w *responseWriter) WriteHeader(statusCode int) {
+func (w *ResponseWriter) WriteHeader(statusCode int) {
 	w.StatusCode = statusCode
 }
 
-func Get(c appengine.Context, urlStr string, urlValues url.Values) (*responseWriter, error) {
+func Get(c appengine.Context, urlStr string, urlValues url.Values) (*ResponseWriter, error) {
 	if enc := urlValues.Encode(); enc != "" {
 		urlStr = fmt.Sprintf("%v?%v", urlStr, enc)
 	}
 	return do(c, "GET", urlStr, "", nil)
 }
 
-func Post(c appengine.Context, urlStr string, bodyType string, body io.Reader) (*responseWriter, error) {
+func Post(c appengine.Context, urlStr string, bodyType string, body io.Reader) (*ResponseWriter, error) {
 	return do(c, "POST", urlStr, bodyType, body)
 }
 
-func Put(c appengine.Context, urlStr string, bodyType string, body io.Reader) (*responseWriter, error) {
+func Put(c appengine.Context, urlStr string, bodyType string, body io.Reader) (*ResponseWriter, error) {
 	return do(c, "PUT", urlStr, bodyType, body)
 }
 
-func Delete(c appengine.Context, urlStr string, urlValues url.Values) (*responseWriter, error) {
+func Delete(c appengine.Context, urlStr string, urlValues url.Values) (*ResponseWriter, error) {
 	if enc := urlValues.Encode(); enc != "" {
 		urlStr = fmt.Sprintf("%v?%v", urlStr, enc)
 	}
 	return do(c, "DELETE", urlStr, "", nil)
 }
 
-func do(c appengine.Context, method, urlStr string, bodyType string, body io.Reader) (*responseWriter, error) {
+func do(c appengine.Context, method, urlStr string, bodyType string, body io.Reader) (*ResponseWriter, error) {
 	r, err := http.NewRequest(method, urlStr, body)
 	if err != nil {
 		return nil, err
@@ -69,7 +69,7 @@ func do(c appengine.Context, method, urlStr string, bodyType string, body io.Rea
 	return Do(c, r)
 }
 
-func Do(c appengine.Context, r *http.Request) (*responseWriter, error) {
+func Do(c appengine.Context, r *http.Request) (*ResponseWriter, error) {
 	r.Header.Set("X-AppEngine-Inbound-AppId", "dev~"+appengine.AppID(c))
 	w := newResponseWriter()
 	http.DefaultServeMux.ServeHTTP(w, r)
